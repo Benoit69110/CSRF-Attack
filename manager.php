a26d8e12c792dc8902dbe0afb344c137365c1d68
Benoit Algourdin <benoit.algourdin@insa-lyon.fr>
2022-03-15 21:56:46 +0100

    indentation

--- manager.php
+++ manager.php
@@ -4,8 +4,8 @@
     $dbName='madb';
     $dbUser='root';
     $dbPwd='root';
-	try{
-        $bdd = new PDO('mysql:host='.$host.';dbname='.$dbName.';charset=utf8', $dbUser, $dbPwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    try{
+      $bdd = new PDO('mysql:host='.$host.';dbname='.$dbName.';charset=utf8', $dbUser, $dbPwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());
+      die('Erreur : ' . $e->getMessage());
     }
